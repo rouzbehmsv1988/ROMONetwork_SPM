6e19bbee03350af624eb46cfb6e61e754403e0b9
rouzbeh <rouzbehmsv1988@gmail.com>
2023-08-04 14:44:50 +0200

    prepared the package

---
+++ Sources/RoMoNetwork/Endpoint.swift
@@ -0,0 +1,52 @@
+//
+//  Endpoint.swift
+//  WeatherAPP
+//
+//  Created by rouzbeh on 31.07.23.
+//
+
+import Foundation
+import Combine
+
+// you can input your baseURL and API Key here inorder to have access to the API service you want
+struct Endpoint {
+    private enum MethodTypes: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "Delete"
+    }
+    private var baseURL: URL
+    private var headers: [String: String]?
+    private var params: [[String:String]] = []
+    private var method: MethodTypes
+    private var path: String = ""
+    var request: URLRequest {
+        guard var component = URLComponents(url: baseURL.appendingPathComponent(path), resolvingAgainstBaseURL: true)
+        else {
+            fatalError("Could not create component")
+        }
+        component.queryItems = queryItems
+        
+        guard let url = component.url else {fatalError("couldnt find url")}
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = headers
+        return request
+    }
+    private var queryItems: [URLQueryItem] {
+        var items: [URLQueryItem] = []
+        for param in params {
+            if let objectKey = param.keys.first, let objectValue = param.values.first {
+                items.append(URLQueryItem(name: objectKey, value: objectValue))
+            }
+        }
+       
+        return items
+    }
+
+    
+    
+    
+}
+
--- Sources/RoMoNetwork/RoMoNetwork.swift
+++ Sources/RoMoNetwork/RoMoNetwork.swift
@@ -1,6 +1,38 @@
-public struct RoMoNetwork {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import Combine
+import Foundation
+
+public class RoMoNetwork<T: Decodable> {
+    deinit {
+        cancellableData = nil
+    }
+    static var shared: RoMoNetwork<T> {
+        return RoMoNetwork<T>()
+    }
+    
+   private var cancellableData: AnyCancellable?
+    func requestPublisher(_ endpoint: Endpoint) -> AnyPublisher<T, Error> {
+        if #available(macOS 10.15, *) {
+            return APIClient().fetchData(endpoint.request).eraseToAnyPublisher()
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    func request(_ endpoint: Endpoint, completionHandler: @escaping (_ data: T?, _ error: Error?) -> Void) {
+        cancellableData = requestPublisher(endpoint).mapError({(error) -> Error in
+            NSLog(error.localizedDescription)
+            return error
+        }).sink(receiveCompletion: { completion
+            in
+            switch completion {
+                case .failure(let error): completionHandler(nil, error)
+                case .finished: NSLog("failed to load data")
+                               }
+        }, receiveValue: {value in
+            completionHandler(value, nil)
+        })
+
     }
 }
+
