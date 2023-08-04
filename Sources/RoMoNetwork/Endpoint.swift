111fa08324e36554e1e30b1ce5c6794b57df6677
rouzbeh <rouzbehmsv1988@gmail.com>
2023-08-04 15:19:06 +0200

    make endpoint accessible

--- Sources/RoMoNetwork/Endpoint.swift
+++ Sources/RoMoNetwork/Endpoint.swift
@@ -10,18 +10,18 @@ import Combine
 
 // you can input your baseURL and API Key here inorder to have access to the API service you want
 public struct ROMOEndpoint {
-    private enum MethodTypes: String {
+    public enum MethodTypes: String {
         case get = "GET"
         case put = "PUT"
         case post = "POST"
         case delete = "Delete"
     }
-    private var baseURL: URL
-    private var headers: [String: String]?
-    private var params: [[String:String]] = []
-    private var method: MethodTypes
-    private var path: String = ""
-    var request: URLRequest {
+    public var baseURL: URL
+    public var headers: [String: String]?
+    public var params: [[String:String]] = []
+    public var method: MethodTypes
+    public var path: String = ""
+    public var request: URLRequest {
         guard var component = URLComponents(url: baseURL.appendingPathComponent(path), resolvingAgainstBaseURL: true)
         else {
             fatalError("Could not create component")
